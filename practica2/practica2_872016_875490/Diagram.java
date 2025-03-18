83bee4b9be38d1affd9291a1f0cd80f704d3e7d4
YeJhao <oni.wm11@gmail.com>
2025-03-18 08:28:07 +0100

    Commit p2, submitted

--- practica2/practica2_872016_875490/Diagram.java
+++ practica2/practica2_872016_875490/Diagram.java
@@ -1,6 +1,6 @@
 //-------------------------------------------------------------------------------------------
 // File:   Diagram.java
-// Author: Jorge Soria Romero (872016) y Jiahao Ye (875490)
+// Author: Jorge Soria Romeo (872016) y Jiahao Ye (875490)
 // Date:   17 de marzo de 2025
 // Coms:   Fichero java de la clase Diagram, de la práctica 2 de Arquitectura Software.
 //-------------------------------------------------------------------------------------------
